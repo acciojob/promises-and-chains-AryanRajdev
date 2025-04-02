1186e8302c41418da0d078085a160b945d2ace7e
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-04-02 20:44:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const mybtn = document.querySelector("#btn");
+
+mybtn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	const name = document.getElementById("name").value;
+	const age = document.getElementById("age").value;
+
+	if(name !=="" && age !==""){
+		const promise1 = ()=>{
+			return new Promise((resolve,reject)=>{
+				
+				setTimeout(()=>{
+					if(Number(age)>=18){
+					resolve(`Welcome,${name}. You can vote.`);
+				   }
+				  else{
+					reject(`Oh sorry ${name}. You aren't old enough.`)
+				  }	
+				},4000)
+				
+			});
+		}
+		promise1()
+		.then((message)=>{
+			alert(message);
+		})
+		.catch((message)=>{
+			alert(message);
+		});
+	}
+	else{
+		alert("Please enter valid details.");
+	}
+});
